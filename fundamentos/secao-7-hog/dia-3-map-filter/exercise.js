79a004900b6edb0c840a7d4e3d55d24ca17697e2
Maria Gabriela Galindo <gabi_who@live.cm>
2022-09-26 19:01:49 -0300

    Adiciona exercicios do dia

---
+++ fundamentos/secao-7-hog/dia-3-map-filter/exercise.js
@@ -0,0 +1,119 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  //exercicio 1
+
+  const bookList = books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+//   console.log(bookList);
+
+  //exercicio 2
+
+  function arrayObjeto(){
+    return books
+  .map((book) => ({
+    author: book.author.name,
+    age: book.releaseYear - book.author.birthYear,
+  }))
+  .sort((a, b) => a.age - b.age);
+}
+
+//   console.log(arrayObjeto(books));
+
+//exercicio 3
+
+const bookGenre = books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+
+// console.log(bookGenre);
+
+//exercicio 4
+
+const bookAge = books.filter((book) => 2022 - book.releaseYear >= 60).sort((a, b) => a.releaseYear - b.releaseYear);
+
+// console.log(bookAge);
+
+//exercicio 5
+
+const authorName = bookGenre
+.map((book) => book.author.name)
+.sort();
+
+// console.log(authorName);
+
+//exercicio 6
+
+const bookName = bookAge.map((book) => book.name);
+
+// console.log(bookName);
+
+//exercicio 7
+
+function authorWithThreeInitials(){ 
+    return books
+.find((book) => (
+    book.author.name[1] === '.' 
+    && book.author.name[4] === '.'
+    && book.author.name[7] === '.'
+)).name
+};
+
+console.log(authorWithThreeInitials(books));
